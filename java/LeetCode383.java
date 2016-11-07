f872ecc104830da48fb34e540a1d6a15beb2b0d3
zybandroid <2103740217@qq.com>
2016-11-07 20:24:29 +0800

    20161107

--- java/LeetCode383.java
+++ java/LeetCode383.java
@@ -24,3 +24,23 @@ public class Solution {
 		return true;
 	}
 }
+//方法二  效率较高
+public class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+	int[] arr = new int[26];
+        for(int i=0;i<magazine.length();i++)
+        {
+            char c = magazine.charAt(i);
+            arr[c-'a']++;
+        }
+        
+        for(int i=0;i<ransomNote.length();i++)
+        {
+            char c = ransomNote.charAt(i);
+            arr[c-'a']--;
+            if(arr[c-'a']<0) return false;
+        }
+        
+        return true;
+	}
+}
